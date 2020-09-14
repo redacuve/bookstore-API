a256bc28b5dcb164b7eff71ba20c1c4568db8f7c
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-09-14 19:29:52 -0300

    Added basic validations for models

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_one(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_numericality_of(:percentage).only_integer.on(:create) }
+  it { should validate_length_of(:percentage).is_at_least(1).is_at_most(100).on(:create) } 
 end
