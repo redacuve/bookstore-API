411a8e168a51ab85c137b30fa34964830bd4485f
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-09-14 22:50:00 -0300

    Added basic test cases. Fixed issues not related to relations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :book
+
+  validates_presence_of :name
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   it { should have_one(:category) }
   it { should validate_presence_of(:title) }
-  it { should validate_numericality_of(:percentage).only_integer.on(:create) }
-  it { should validate_length_of(:percentage).is_at_least(1).is_at_most(100).on(:create) } 
+  it { should validate_numericality_of(:percentage).is_less_than_or_equal_to(100).is_greater_than_or_equal_to(1) }
 end
