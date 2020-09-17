2a1d795b06764397cf56f8ac0a2ea0f196d17202
Rey David Cuevas <cuevas.vela@gmail.com>
2020-09-17 16:08:22 -0500

    updated authenticate controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  scope module: :v2, constraints: ApiVersion.new('v2', true) do
+    resources :books, only: :index
+  end
+
   scope module: :v1, constraints: ApiVersion.new('v1', true) do
     resources :books
   end
+
   post 'signup', to: 'users#create'
+  post 'auth/login', to: 'authentication#authenticate'
 end
