47d47a2314bcc247658200ab500d22598b4a10e7
Rey David Cuevas <cuevas.vela@gmail.com>
2020-09-17 16:08:30 -0500

    lint files

--- app/lib/api_version.rb
+++ app/lib/api_version.rb
@@ -1,7 +1,7 @@
 class ApiVersion
   attr_reader :version, :default
 
-  def initialize(version, default= false)
+  def initialize(version, default = false)
     @version = version
     @default = default
   end
--- spec/requests/v2/books_request_spec.rb
+++ spec/requests/v2/books_request_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "V2::Books", type: :request do
-
 end
